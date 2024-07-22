e8fd8bfa4ec10faf1d949ac30868bb8ac8ccb04c
Ataa17 <ataa.mars@yahoo.com>
2024-07-22 15:28:20 +0100

    Table api secured using token auth method, endpoints tested and working

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics ,viewsets
 from .models import menu,booking
 from .serializers import MenuSerializer , BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 class MenuItemsView(generics.ListCreateAPIView):
     queryset=menu.objects.all()
@@ -10,7 +11,7 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = menu.objects.all()
     serializer_class = MenuSerializer
-
 class bookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset=booking.objects.all()
     serializer_class=BookingSerializer
